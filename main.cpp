5ddcad0cc822896f5cb4aa2588a094936b4df4af
20171105111 <2604406667@qq.com>
2018-07-10 14:55:04 +0800

    text 7.10
    最终

--- main.cpp
+++ main.cpp
@@ -3,11 +3,10 @@
 #include <sstream>
 #include <string>
 #include <vector>
-#include <cstdlib>
 
 using namespace std;
 
-string Trim(string& str)
+string Trim(string& str)//str.find_first_not_of(" \t\r\n"),在字符串str中从索引0开始，返回首次不匹配"\t\r\n"的位置
 {
     str.erase(0,str.find_first_not_of(" \t\r\n"));
     str.erase(str.find_last_not_of(" \t\r\n") + 1);
@@ -20,16 +19,16 @@ int main()
     int a[10][10];
     string stu[20][100];
     double sum[50][50];
-    ifstream fin("/Users/s20171105111/Desktop/text708/text708/File.csv");
+    ifstream fin("/Users/s20171105111/Desktop/text708/text708/File.csv");//打开文件
     string line;
     while (getline(fin, line))
     {
-        istringstream sin(line);
+        istringstream sin(line);//将整行字符串line读入到字符串流istringstream中
         vector<string> fields;
         string field;
         while (getline(sin, field, ','))
         {
-            fields.push_back(field);
+            fields.push_back(field);//读取逗号
         }
         stu[0][i] = Trim(fields[0]);
         stu[1][i] = Trim(fields[1]);
@@ -54,7 +53,7 @@ int main()
             c<<stu[j+6][i+1];
             c>>a[i][j];
         }
-    }
+    }//把字符串转换int 类型
     for(t=0;t<N;t++)
     {
         for (i=0;i<5-1;i++)
@@ -70,10 +69,10 @@ int main()
                 }
             }
         }
-    }
+    }//一个人成绩从大到小排序
     for(i=0;i<N;i++)
     {
-        sum[i][i]=0;
+        sum[i][i]=0;//成绩
         for(j=1;j<5-1;j++)
         {
             
@@ -83,7 +82,7 @@ int main()
     }
     ofstream ofile;
     ofile.open("/Users/s20171105111/Desktop/text708/text708/Fileout.csv",ios::out | ios::trunc);
-    ofile<<"number,name,sex,birthdate,class,phonenumber,judge"<<endl;
+    ofile<<"number,name,sex,birthdate,class,phonenumber,average"<<endl;
     for(i=0;i<=N-1;i++)
     {
         ofile<<stu[0][i+1];
@@ -94,6 +93,6 @@ int main()
         ofile<<","<<sum[i][i];
         ofile<<endl;
     }
-    ofile.close();
+    ofile.close();//关闭文件
     return 0;
 }
